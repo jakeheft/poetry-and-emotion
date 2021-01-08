f8bec54d4bc0027b66ad13ced5425431111f986e
Jake Heft <jakeheft@gmail.com>
2021-01-08 10:44:06 -0700

    Add memoization to limit API calls

--- app/poros/poem.rb
+++ app/poros/poem.rb
@@ -1,9 +1,13 @@
 class Poem
 	attr_reader :title, :author, :poem_text
-	
+
 	def initialize(data)
 		@title = data[:title]
 		@author = data[:author]
 		@poem_text = data[:lines].join
+	end
+
+	def tones
+		@tones ||= ToneFacade.find_tone(poem_text)
 	end
 end
